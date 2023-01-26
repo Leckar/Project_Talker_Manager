8026e0bfc3557c1cdb8ba487cd3dc7d9fb07b3ea
Leckar <gusleckar@gmail.com>
2023-01-25 22:10:34 -0300

    req 2 done

--- src/routers/talker.router.js
+++ src/routers/talker.router.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const { readFile } = require('../utils/fs');
-const { OK_STATUS } = require('../utils/httpStatuses');
+const { OK_STATUS, NOT_FOUND_STATUS } = require('../utils/httpStatuses');
 
 const router = express.Router();
 
 router.get('/talker', async (_req, res) => {
   const talkers = await readFile();
   return res.status(OK_STATUS).json(talkers);
+});
+
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readFile();
+  const targetTalker = talkers.find((t) => t.id === Number(id));
+  if (!targetTalker) {
+    return res.status(NOT_FOUND_STATUS)
+      .json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(OK_STATUS).json(targetTalker);
 });
 
 module.exports = router;
--- src/utils/httpStatuses.js
+++ src/utils/httpStatuses.js
@@ -1,5 +1,7 @@
 const OK_STATUS = 200;
+const NOT_FOUND_STATUS = 404;
 
 module.exports = {
   OK_STATUS,
+  NOT_FOUND_STATUS,
 };
