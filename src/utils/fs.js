a5700a5f74d494e8013dd7472f225b7e5dea55e7
Leckar <gusleckar@gmail.com>
2023-01-25 21:37:16 -0300

    setting up fs

--- src/utils/fs.js
+++ src/utils/fs.js
@@ -5,8 +5,8 @@ const FILE_PATH = path.resolve(__dirname, '../talker.json');
 
 const readFile = async () => {
   const data = await fs.readFile(FILE_PATH);
-  const parsed = await JSON.parse(data);
-  return parsed;
+  const parsedData = await JSON.parse(data);
+  return parsedData;
 };
 
 module.exports = {
